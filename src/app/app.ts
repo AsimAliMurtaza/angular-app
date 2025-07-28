1a7fb34507f055cb20d58bb5d1a8b0d85873962d
Asim Ali Murtaza <mo.asim.murtaza@gmail.com>
2025-07-28 12:01:07 +0500

    added dynamic routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,11 @@
 import { Component } from '@angular/core';
-import { Home } from './home/home';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Home],
-  template: `
-    <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
-      </header>
-      <section class="content">
-        <app-home></app-home>
-      </section>
-    </main>
-  `,
+  imports: [RouterModule],
+  templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {

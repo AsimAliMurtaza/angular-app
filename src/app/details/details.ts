b462c00db230c589d166e1dea5f84a6c2cf43763
Asim Ali Murtaza <mo.asim.murtaza@gmail.com>
2025-07-28 12:27:33 +0500

    Added filters, rest api for data fetching

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Housing } from '../housing';
 import { HousingLocationInfo } from '../housinglocation';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './details.html',
   styleUrl: './details.scss',
 })
@@ -13,9 +14,34 @@ export class Details {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(Housing);
   housingLocation: HousingLocationInfo | undefined;
+
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
+
   constructor() {
-    const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation =
-      this.housingService.getHousingLocationById(housingLocationId);
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+    this.housingService
+      .getHousingLocationById(housingLocationId)
+      .then((housingLocation) => {
+        this.housingLocation = housingLocation;
+      });
+  }
+
+  submitApplication() {
+    if (this.applyForm.valid && this.housingLocation) {
+      const { firstName, lastName, email } = this.applyForm.value;
+      this.housingService.submitApplication(
+        firstName ?? '',
+        lastName ?? '',
+        email ?? ''
+      );
+      alert('Application submitted successfully!');
+      this.applyForm.reset();
+    } else {
+      alert('Please fill out all fields correctly.');
+    }
   }
 }
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -11,9 +11,26 @@ import { HousingLocationInfo } from '../housinglocation';
 })
 export class Home {
   housingLocationList: HousingLocationInfo[] = [];
+  filteredLocationList: HousingLocationInfo[] = [];
   housingService: Housing = inject(Housing);
 
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+
   constructor() {
-    this.housingLocationList = this.housingService.getHousingLocationList();
+    this.housingService
+      .getHousingLocationList()
+      .then((housingLocationList: HousingLocationInfo[]) => {
+        this.housingLocationList = housingLocationList;
+        this.filteredLocationList = housingLocationList;
+      });
   }
 }
